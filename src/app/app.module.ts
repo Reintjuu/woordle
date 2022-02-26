6e497c951a7c47472f2f596b0ca46b297f3faaed
Reinier de Vries <reiniervries@outlook.com>
2022-02-26 04:57:59 +0100

    Added favicon and build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MoveByMaxLengthOrEmptyDirective } from "./move-by-max-length-or-empty.d
 import { TitleComponent } from './title/title.component';
 import { NzButtonModule } from "ng-zorro-antd/button";
 import { NzIconModule } from "ng-zorro-antd/icon";
+import { ReloadOutline } from '@ant-design/icons-angular/icons';
 
 registerLocaleData(nl);
 
@@ -30,7 +31,7 @@ registerLocaleData(nl);
     NzInputModule,
     NzGridModule,
     NzButtonModule,
-    NzIconModule
+    NzIconModule.forRoot([ReloadOutline])
   ],
   providers: [{ provide: NZ_I18N, useValue: nl_NL }],
   bootstrap: [AppComponent]
