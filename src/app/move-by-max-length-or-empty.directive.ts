38e08fb4be5dc88bd1d95f6f801e8122c51e0167
Reinier de Vries <reiniervries@outlook.com>
2022-02-26 18:13:43 +0100

    Made input handling a little faster by using the keydown event instead of the keyup event

--- src/app/move-by-max-length-or-empty.directive.ts
+++ src/app/move-by-max-length-or-empty.directive.ts
@@ -1,43 +1,61 @@
 import { Directive, HostListener } from '@angular/core';
+import { NgModel } from "@angular/forms";
 
 @Directive({
   selector: 'input[moveNextByMaxLength], textarea[moveNextByMaxLength]',
 })
 export class MoveByMaxLengthOrEmptyDirective {
-  private static getNextOrPreviousElement(element: Element | null, goToNext: boolean): Element | null {
+
+  constructor(
+    private ngModel: NgModel) {}
+
+  private static getNextOrPreviousElement(element: HTMLInputElement | null, goToNext: boolean): HTMLInputElement | null {
     if (!element) {
       return null;
     }
 
-    return goToNext ? element.nextElementSibling : element.previousElementSibling;
+    const previousInput = element.previousElementSibling as HTMLInputElement;
+    if (!goToNext && !previousInput?.readOnly) {
+      return previousInput;
+    }
+
+    if (goToNext) {
+      return element.nextElementSibling as HTMLInputElement;
+    }
+
+    return null;
   }
 
-  @HostListener('keyup', ['$event']) onKeyDown(keyboardEvent: KeyboardEvent) {
+  @HostListener('keydown', ['$event']) onKeyDown(keyboardEvent: KeyboardEvent) {
     const target = keyboardEvent.target as
       | HTMLInputElement
-      | HTMLTextAreaElement
       | null;
 
-    const goToNext = keyboardEvent.code !== 'Backspace';
-    if (!target
-      || keyboardEvent.code === 'Tab'
-      || (goToNext && target.maxLength !== target.value.length)) {
+    const isBackspace = keyboardEvent.code === 'Backspace';
+    const isCharacterKey = keyboardEvent.key.length === 1;
+    if (!target || keyboardEvent.code === 'Tab' || (!isBackspace && !isCharacterKey)) {
       return;
     }
 
     keyboardEvent.preventDefault();
 
-    let selectedSibling = MoveByMaxLengthOrEmptyDirective.getNextOrPreviousElement(target, goToNext)
+    // It's a character key.
+    if (isCharacterKey) {
+      target.value = keyboardEvent.key;
+      this.ngModel.update.emit(target.value);
+    }
+
+    let selectedSibling = MoveByMaxLengthOrEmptyDirective.getNextOrPreviousElement(target, !isBackspace)
 
     while (selectedSibling) {
-      const firstInputChild = selectedSibling as HTMLInputElement | HTMLTextAreaElement;
+      const firstInputChild = selectedSibling as HTMLInputElement;
       if (firstInputChild.type === target.type) {
+        firstInputChild.value = '';
         firstInputChild.focus();
-        firstInputChild.select();
         return;
       }
 
-      selectedSibling = MoveByMaxLengthOrEmptyDirective.getNextOrPreviousElement(selectedSibling, goToNext);
+      selectedSibling = MoveByMaxLengthOrEmptyDirective.getNextOrPreviousElement(selectedSibling, !isBackspace);
     }
   }
 }
